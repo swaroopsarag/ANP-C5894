f490de75906483eff6b6eb3e8530524a2733b603
swaroopsarag <128209080+swaroopsarag@users.noreply.github.com>
2023-07-03 11:45:39 +0530

    Add files via upload

--- practile/HelloWorld.java
+++ practile/HelloWorld.java
@@ -1,10 +1,8 @@
-package oops;
-
- class HelloWorld {
-
-	public static void main(String[] args) {
-    System.out.println ("Hello World");
-
-	}
-
+//to print simple "Hello World"
+class HelloWorld
+{
+public static void main (String[]args)
+{
+System.out.println("Hello Java World");
+}
 }
